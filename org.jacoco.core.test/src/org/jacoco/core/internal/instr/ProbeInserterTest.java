853fbe748ed4778a2dca70e15cb508717e05b3ba
Pieter Dirk Soels <p.d.soels@hotmail.com>
2021-07-28 10:17:09 +0200

    Add line execution count capped by using Math.min

--- org.jacoco.core.test/src/org/jacoco/core/internal/instr/ProbeInserterTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/instr/ProbeInserterTest.java
@@ -66,8 +66,14 @@ public class ProbeInserterTest {
 
 		expectedVisitor.visitVarInsn(Opcodes.ALOAD, 0);
 		expectedVisitor.visitInsn(Opcodes.ICONST_0);
+		expectedVisitor.visitInsn(Opcodes.DUP2);
+		expectedVisitor.visitInsn(Opcodes.IALOAD);
 		expectedVisitor.visitInsn(Opcodes.ICONST_1);
-		expectedVisitor.visitInsn(Opcodes.BASTORE);
+		expectedVisitor.visitInsn(Opcodes.IADD);
+		expectedVisitor.visitLdcInsn(Integer.MAX_VALUE - 1);
+		expectedVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math",
+				"min", "(II)I", false);
+		expectedVisitor.visitInsn(Opcodes.IASTORE);
 	}
 
 	@Test
@@ -78,8 +84,14 @@ public class ProbeInserterTest {
 
 		expectedVisitor.visitVarInsn(Opcodes.ALOAD, 1);
 		expectedVisitor.visitInsn(Opcodes.ICONST_0);
+		expectedVisitor.visitInsn(Opcodes.DUP2);
+		expectedVisitor.visitInsn(Opcodes.IALOAD);
 		expectedVisitor.visitInsn(Opcodes.ICONST_1);
-		expectedVisitor.visitInsn(Opcodes.BASTORE);
+		expectedVisitor.visitInsn(Opcodes.IADD);
+		expectedVisitor.visitLdcInsn(Integer.MAX_VALUE - 1);
+		expectedVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math",
+				"min", "(II)I", false);
+		expectedVisitor.visitInsn(Opcodes.IASTORE);
 	}
 
 	@Test
@@ -90,8 +102,14 @@ public class ProbeInserterTest {
 
 		expectedVisitor.visitVarInsn(Opcodes.ALOAD, 4);
 		expectedVisitor.visitInsn(Opcodes.ICONST_0);
+		expectedVisitor.visitInsn(Opcodes.DUP2);
+		expectedVisitor.visitInsn(Opcodes.IALOAD);
 		expectedVisitor.visitInsn(Opcodes.ICONST_1);
-		expectedVisitor.visitInsn(Opcodes.BASTORE);
+		expectedVisitor.visitInsn(Opcodes.IADD);
+		expectedVisitor.visitLdcInsn(Integer.MAX_VALUE - 1);
+		expectedVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math",
+				"min", "(II)I", false);
+		expectedVisitor.visitInsn(Opcodes.IASTORE);
 	}
 
 	@Test
@@ -102,8 +120,15 @@ public class ProbeInserterTest {
 
 		expectedVisitor.visitVarInsn(Opcodes.ALOAD, 5);
 		expectedVisitor.visitInsn(Opcodes.ICONST_0);
+		expectedVisitor.visitInsn(Opcodes.DUP2);
+		expectedVisitor.visitInsn(Opcodes.IALOAD);
 		expectedVisitor.visitInsn(Opcodes.ICONST_1);
-		expectedVisitor.visitInsn(Opcodes.BASTORE);
+		expectedVisitor.visitInsn(Opcodes.IADD);
+		expectedVisitor.visitLdcInsn(Integer.MAX_VALUE - 1);
+		expectedVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math",
+				"min", "(II)I", false);
+		expectedVisitor.visitInsn(Opcodes.IASTORE);
+
 	}
 
 	@Test
@@ -227,7 +252,7 @@ public class ProbeInserterTest {
 		pi.visitMaxs(10, 8);
 
 		expectedVisitor.visitLdcInsn("init");
-		expectedVisitor.visitMaxs(13, 9);
+		expectedVisitor.visitMaxs(14, 9);
 	}
 
 	@Test
@@ -240,7 +265,7 @@ public class ProbeInserterTest {
 				new Object[0]);
 
 		expectedVisitor.visitFrame(Opcodes.F_NEW, 4,
-				new Object[] { "Foo", Opcodes.LONG, "[Z", "java/lang/String" },
+				new Object[] { "Foo", Opcodes.LONG, "[I", "java/lang/String" },
 				0, new Object[0]);
 	}
 
@@ -251,7 +276,7 @@ public class ProbeInserterTest {
 
 		pi.visitFrame(Opcodes.F_NEW, 0, new Object[] {}, 0, new Object[0]);
 
-		expectedVisitor.visitFrame(Opcodes.F_NEW, 1, new Object[] { "[Z" }, 0,
+		expectedVisitor.visitFrame(Opcodes.F_NEW, 1, new Object[] { "[I" }, 0,
 				new Object[0]);
 	}
 
@@ -264,7 +289,7 @@ public class ProbeInserterTest {
 				0, new Object[0]);
 
 		expectedVisitor.visitFrame(Opcodes.F_NEW, 3,
-				new Object[] { "[Z", Opcodes.DOUBLE, "Foo" }, 0, new Object[0]);
+				new Object[] { "[I", Opcodes.DOUBLE, "Foo" }, 0, new Object[0]);
 	}
 
 	@Test
@@ -276,7 +301,7 @@ public class ProbeInserterTest {
 
 		// The locals in this frame are filled with TOP up to the probe variable
 		expectedVisitor.visitFrame(Opcodes.F_NEW, 2,
-				new Object[] { Opcodes.TOP, "[Z", }, 0, new Object[] {});
+				new Object[] { Opcodes.TOP, "[I", }, 0, new Object[] {});
 	}
 
 	@Test
@@ -288,7 +313,7 @@ public class ProbeInserterTest {
 
 		// The locals in this frame are filled with TOP up to the probe variable
 		expectedVisitor.visitFrame(Opcodes.F_NEW, 3,
-				new Object[] { Opcodes.TOP, Opcodes.TOP, "[Z", }, 0,
+				new Object[] { Opcodes.TOP, Opcodes.TOP, "[I", }, 0,
 				new Object[] {});
 	}
 
@@ -304,7 +329,7 @@ public class ProbeInserterTest {
 		expectedVisitor
 				.visitFrame(
 						Opcodes.F_NEW, 5, new Object[] { Opcodes.DOUBLE,
-								Opcodes.TOP, Opcodes.TOP, Opcodes.TOP, "[Z", },
+								Opcodes.TOP, Opcodes.TOP, Opcodes.TOP, "[I", },
 						0, new Object[] {});
 	}
 
